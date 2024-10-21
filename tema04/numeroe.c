1c834cfb3986123bbff517a85ea8c0729c8fcd25
MREBOLLO <mrebollo@upvnet.upv.es@ldsic-vdi13.upvnet.upv.es>
2024-10-21 11:01:46 +0200

    ajuste bucle numeroe

--- tema04/numeroe.c
+++ tema04/numeroe.c
@@ -7,18 +7,27 @@
 
 int main(int argc, char *argv[] ){
     int n;
-    double e = 0;
     //obtener el número de términos de la sucesión
     n = atoi(argv[1] );
 
     //calcular la aproximación de e
     int fact_i = 1;
-    for(int i = 0; i < n; i++){
+    /*
+      NOTA:
+      El bucle empieza en 1 para evitar fact_i *= 0
+      Al "sacar" el primer término 1/0! del bucle,
+      hay que inicializar e a 1 (1/0! = 1/1) 
+    */
+    double e = 1;
+    for(int i = 1; i < n; i++){
         //fact_i = fact_i * i;
         fact_i *= i;
         e += 1 / (double)fact_i;
         //e = e + 1.0 / fact_i
     } 
+
+    // para imprimir un double (un float largo) se emplea %lf
+    print("e = %lf\n ", e);
 
     return 0;
 } 
